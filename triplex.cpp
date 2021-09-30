d6c21c957211bc707e97a589a57d7a4cc0ff27af
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-09-30 18:53:24 -0500

    Starting triplex Game

---
+++ triplex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secrete agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
+
+    int a = 4;
+    int b = 5;
+    int c = 7;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
+    return 0;
+}
