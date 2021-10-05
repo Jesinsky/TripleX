dc485a77b8fd51d5ac232ccc0756d13ca5217c2c
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-05 17:11:14 -0500

    Adding comments
    Adding comments and declaring constants to the variables

--- triplex.cpp
+++ triplex.cpp
@@ -2,17 +2,20 @@
 
 int main()
 {
+    // Print welcome message to the terminal.
     std::cout << "You are a secrete agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
 
-    int a = 4;
-    int b = 5;
-    int c = 7;
+    // Declare 3 number code.
+    const int a = 4;
+    const int b = 5;
+    const int c = 7;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
+    // Print sum and product to the terminal.
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
